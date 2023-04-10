c81ea5e6471d66e3ccf37a7fdbd1c0db4c30f4d7
TDisra <52383124+TDisra@users.noreply.github.com>
2023-04-10 23:45:42 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+
+if __name__ == '__main__':
+    c = Connector(
+        mainPath= input("Enter Monitored path (e.g C:\HarmonyConnector)"),
+        clientID=input("Enter Client id (e.g 457547568856i56u7oi3576658"),
+        secretID=input(Enter "secret id (e.g 3416344357536wqet346543675)",
+        url=input("Enter url (e.g https://cloudinfra-us.portal.checkpoint.com/auth/external)"
+    )
+    c.startMonitoring()
